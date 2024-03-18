90eab8def345e487e8b3e3156c08123277a68394
habib-hb <developerhabib1230@gmail.com>
2024-03-18 11:10:38 +0600

    Search Functionality added (it's quite awesome :)

--- js/searchHandling.js
+++ js/searchHandling.js
@@ -1,6 +1,8 @@
 const searchInput = document.getElementById("search");
 const searchBtn = document.getElementById("search-btn");
+const dropdownContent = document.querySelector(".dropdown-content");
 
+//redirect after search and button click------
 searchBtn.addEventListener("click" , ()=>{
     const searchValue = searchInput.value;
     console.log("search value : " , searchValue);
@@ -28,4 +30,52 @@ searchBtn.addEventListener("click" , ()=>{
             break;
     
         }
+})
+
+//filtering options by input value--------
+searchInput.addEventListener("input" , function(){
+    let inputText = searchInput.value.toLowerCase();
+    let options = document.querySelectorAll(".dropdown-content a");
+    options.forEach((item)=>{
+        let itemText = item.textContent.toLowerCase();
+        if(itemText.includes(inputText)){
+            item.style.display = "block";
+        }
+        else{
+            item.style.display = "none";
+        }
+    })
+    dropdownContent.style.display = "block";
+        
+})
+
+//redirect based on the input value after "Enter" press -------- 
+searchInput.addEventListener("keydown" , (e)=>{
+    if(e.key==="Enter"){
+        let searchValue = searchInput.value.toLowerCase();
+        let options = document.querySelectorAll(".dropdown-content a");
+        options.forEach((item)=>{
+            let itemText = item.textContent.toLowerCase();
+            let foundFirstValue = false;
+            if(itemText.includes(searchValue) && !foundFirstValue) {
+                foundFirstValue = true;
+                window.location.hash = itemText=== "contact us" ? "contact" : itemText;
+                dropdownContent.style.display = "none";
+                searchInput.value = "";
+             }
+
+        })
+    }
+})
+
+//showing dropdown after clicking the input section -------
+searchInput.addEventListener("click" , ()=>{
+     dropdownContent.style.display = "block";
+})
+
+//hide dropdown after clicking outside of it -------- 
+window.addEventListener("click" , (e)=>{
+    if(!e.target.matches("#search")){
+    dropdownContent.style.display = "none";
+    }
 })
